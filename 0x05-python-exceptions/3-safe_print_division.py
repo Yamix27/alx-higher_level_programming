b7eab6d7da406aa5feb08441e4b7a858a7aebe1e
Your Username <amyne.boutallagh@gmail.com>
2023-10-23 15:40:31 -0700

    y

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
-
     try:
         result = a / b
-    except ZeroDivisionError:
+        print("Inside result: {:.1f}".format(result))
+    except:
         result = None
+        print("Inside result: {}".format(result))
     finally:
-        print('Inside result: ()'.format(result))
         return result
