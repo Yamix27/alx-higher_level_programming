d5622aa73b65de7cef6155a2bfebb364cba0eb77
Your Username <amyne.boutallagh@gmail.com>
2024-01-25 12:02:25 -0800

    task7_fixed

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL and displays only the status code of the response
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-echo "$response"
-
+curl -sI "$1" | grep -i "HTTP" | awk '{print $2}'
