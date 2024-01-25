52ebf01489d9adcae9c88ba386e9b692957e5ad3
Your Username <amyne.boutallagh@gmail.com>
2024-01-25 12:10:03 -0800

    advanced task fixed

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL with the contents of a file in the body and displays the response body
-
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <URL> <JSON_FILE>"
-    exit 1
-fi
-
-url="$1"
-json_file="$2"
-
-# Check if the file is a valid JSON
-if ! jq '.' "$json_file" >/dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-curl -sX POST -H "Content-Type: application/json" -d @"$json_file" "$url"
+# curl a json file
+curl -s -H "content-type:application/json"  -d @"$2" -X POST "$1"
