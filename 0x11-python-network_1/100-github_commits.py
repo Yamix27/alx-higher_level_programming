7742b4e8c777708c178d68d15085bb1fb69e9040
Your Username <amyne.boutallagh@gmail.com>
2024-01-26 08:44:18 -0800

    task10Fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,15 +14,11 @@ if __name__ == "__main__":
 
     url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
 
-    # Fetch the commit data from the GitHub API
     response = requests.get(url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Parse the JSON response
         commits = response.json()
 
-        # Display the 10 most recent commits
         for commit in commits[:10]:
             sha = commit.get('sha')
             author_name = commit.get('commit').get('author').get('name')
