91cd1c763307391d8e7486bc7de667a720094796
Your Username <amyne.boutallagh@gmail.com>
2024-02-20 15:48:53 -0800

    Done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
+// read and print the content of a file.
 
 const fs = require('fs');
 
 const filePath = process.argv[2];
 
 try {
-	  const data = fs.readFileSync(filePath, 'utf-8');
-	  console.log(data);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(data);
 } catch (err) {
-	  console.log(err);
+  console.log(err);
 }
