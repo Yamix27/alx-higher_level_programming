b77ea6d685936da6e68df9a11307881b5993ed86
Your Username <amyne.boutallagh@gmail.com>
2024-02-20 16:24:06 -0800

    Done

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,12 +7,12 @@ const URL = 'https://swapi-api.hbtn.io/api/films/';
 const episodeNbr = process.argv[2];
 
 request(URL + episodeNbr, (err, response, body) => {
-if (err) {
-console.log(err);
-} else if (response.statusCode === 200) {
-const responseJSON = JSON.parse(body);
-console.log(responseJSON.title);
-} else {
-console.log('Error code: ' + response.statusCode);
-}
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
