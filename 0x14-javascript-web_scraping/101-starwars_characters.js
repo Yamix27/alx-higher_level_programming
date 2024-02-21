7ba621ca45c1d29b8188701536c414cadfe2ee8f
Your Username <amyne.boutallagh@gmail.com>
2024-02-20 16:42:51 -0800

    Done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-	if (!error) {
-		const characters = JSON.parse(body).characters;
-		printCharacters(characters, 0);
-	}
-});
+// print all characters of a Star Wars movie.
 
-function printCharacters (characters, index) {
-	request(characters[index], function (error, response, body) {
-		if (!error) {
-			console.log(JSON.parse(body).name);
-			if (index + 1 < characters.length) {
-				printCharacters(characters, index + 1);
-			}
-		}
-	});
-}
+const util = require('util');
+const request = require('request');
+const id = process.argv[2];
+
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const body = (await prequest(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true })).body;
+    for (const url of body.characters) {
+      const body = (await prequest(url, { json: true })).body;
+      console.log(body.name);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
