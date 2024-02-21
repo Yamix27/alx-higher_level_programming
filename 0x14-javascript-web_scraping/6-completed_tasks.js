aa936c062e095e5ce84a6591b01d8aefb671de0b
Your Username <amyne.boutallagh@gmail.com>
2024-02-20 16:30:47 -0800

    Done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,19 @@
 #!/usr/bin/node
+// computes the number of tasks completed by user id.
 
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, response, body) {
+request(url, { json: true }, (err, res, body) => {
   if (err) {
-  console.log(err);
-} else if (response.statusCode === 200) {
-const completed = {};
-const tasks = JSON.parse(body);
-for (const i in tasks) {
-const task = tasks[i];	
-  if (task.completed === true) {
-  if (completed[task.userId] === undefined) {
-completed[task.userId] = 1;
-} else {
-completed[task.userId]++;
+    console.log(err);
 }
-}
-}
-console.log(completed);
-} else {
-console.log('An error occured. Status code: ' + response.statusCode);
-}
+
+  const results = {};
+  for (const task of body) {
+    if (task.completed) {
+      results[task.userId] = (results[task.userId] || 0) + 1;
+    }
+  }
+  console.log(results);
 });
